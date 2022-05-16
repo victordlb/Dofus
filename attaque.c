b32719095cae0c1e2f52f53485bce1dbf9bf84c9
PaulNougarede <paul.nougarede78@gmail.com>
2022-05-16 19:57:29 +0200

    Image sort
    image pour franky

--- attaque.c
+++ attaque.c
@@ -3,12 +3,8 @@
 #include <allegro.h>
 #include "header.h"
 
-void chargerSort(t_joueur** tabjoueur, int indice)
-{
-
-}
-
 void combat(t_cases** tabcases, t_joueur** tabjoueur, int indice, int nbrjoueur, BITMAP* fond)
 {
+
 
 }
