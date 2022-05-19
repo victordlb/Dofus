ff8cf1b09e20e306de5ddf71ade6ade11db87b7c
PaulNougarede <paul.nougarede78@gmail.com>
2022-05-19 16:30:13 +0200

    Début combat
    Commencement des combats

--- attaque.c
+++ attaque.c
@@ -5,7 +5,61 @@
 
 void combat(t_cases** tabcases, t_joueur** tabjoueur, int indice, int nbrjoueur, BITMAP* fond)
 {
+    afficherSort(tabjoueur, indice, fond);
+    int done = 0;
+    BITMAP* select = load_bitmap("documents/props/css.bmp", NULL);
+    while(done == 0)
+    {
+        if(mouse_b&1)
+        {
+            if(mouse_x>502 && mouse_x<572 && mouse_y>737 && mouse_y<800)
+            {
+                draw_sprite(fond, select, 495, 730);
+                blit(fond, screen, 0,0,0,0,SCREEN_W, SCREEN_H);
+                porteSort(tabcases, tabjoueur, indice, 0, fond);
+                Sleep(1000);
+                done =1;
 
+            }
+            if(mouse_x>577 && mouse_x<647 && mouse_y>737 && mouse_y<800)
+            {
+                draw_sprite(fond, select, 495 + (1*75), 730);
+                blit(fond, screen, 0,0,0,0,SCREEN_W, SCREEN_H);
+                porteSort(tabcases, tabjoueur, indice, 1, fond);
+                Sleep(1000);
+                done =1;
+            }
+            if(mouse_x>652 && mouse_x<722 && mouse_y>737 && mouse_y<800)
+            {
+                draw_sprite(fond, select, 495 + (2*75), 730);
+                blit(fond, screen, 0,0,0,0,SCREEN_W, SCREEN_H);
+                porteSort(tabcases, tabjoueur, indice, 2, fond);
+                Sleep(1000);
+                done =1;
+            }
+            if(mouse_x>727 && mouse_x<797 && mouse_y>737 && mouse_y<800)
+            {
+                draw_sprite(fond, select, 495 + (3*75), 730);
+                blit(fond, screen, 0,0,0,0,SCREEN_W, SCREEN_H);
+                porteSort(tabcases, tabjoueur, indice, 3, fond);
+                Sleep(1000);
+                done =1;
+            }
+            if(mouse_x>802 && mouse_x<872 && mouse_y>737 && mouse_y<800)
+            {
+                draw_sprite(fond, select, 495 + (4*75), 730);
+                blit(fond, screen, 0,0,0,0,SCREEN_W, SCREEN_H);
+                Sleep(1000);
+                done =1;
+            }
+            if(mouse_x > 1250 && mouse_x < 1300 && mouse_y >750 && mouse_y < 800)
+            {
+                done = 1;
+            }
+        }
+    }
+    Sleep(1000);
+    destroy_bitmap(select);
 }
 
 
@@ -15,6 +69,7 @@ void afficherSort(t_joueur** tabjoueur, int indice, BITMAP* fond)
     BITMAP* sort2;
     BITMAP* sort3;
     BITMAP* sort4;
+    BITMAP* poing = load_bitmap("documents/perso/poing.bmp", NULL);
     if(strcmp(tabjoueur[indice]->classes.nom, "luffy") == 0)
     {
         sort1 = load_bitmap("documents/perso/luffy/sortL/1.bmp", NULL);
@@ -49,9 +104,36 @@ void afficherSort(t_joueur** tabjoueur, int indice, BITMAP* fond)
     draw_sprite(fond, sort2, 502 + (1*75), 737);
     draw_sprite(fond, sort3, 502 + (2*75), 737);
     draw_sprite(fond, sort4, 502 + (3*75), 737);
+    draw_sprite(fond, poing, 502 + (4*75), 737);
+    blit(fond, screen, 0,0,0,0,SCREEN_W, SCREEN_H);
     clear_bitmap(sort1);
     clear_bitmap(sort2);
     clear_bitmap(sort3);
     clear_bitmap(sort4);
+    clear_bitmap(poing);
+}
+
+void porteSort(t_cases** tabcases, t_joueur** tabjoueur, int indice, int numsort, BITMAP* fond)
+{
+    int paraTab[16][28] = {0};
+    if(tabjoueur[indice]->classes.mesattaques[numsort].type == 1)
+    {
+        for(int i = 1; i<=tabjoueur[indice]->classes.mesattaques[numsort].porte; i++)
+        {
+            paraTab[tabjoueur[indice]->classes.cord_y-i][tabjoueur[indice]->classes.cord_x] = 1;
+            paraTab[tabjoueur[indice]->classes.cord_y+i][tabjoueur[indice]->classes.cord_x] = 1;
+            paraTab[tabjoueur[indice]->classes.cord_y][tabjoueur[indice]->classes.cord_x-i] = 1;
+            paraTab[tabjoueur[indice]->classes.cord_y][tabjoueur[indice]->classes.cord_x+i] = 1;
+        }
+        for(int j = 0; j<16; j++)
+        {
+            for(int g = 0; g<28; g++)
+            {
+                if(paraTab[j][g] == 1 && tabcases[j][g].obstacle == 0)
+                    rectfill(fond, tabcases[j][g].x+5, tabcases[j][g].y+5, tabcases[j][g].x+45,tabcases[j][g].y+45, makecol(50,130,246));
+            }
+        }
+
+    }
 }
 
